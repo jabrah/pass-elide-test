22dd4b2e76534399e2fd2b58cc95267316108fce
John Abrahams <jabrah20@jhu.edu>
2022-09-01 14:33:19 -0400

    Update policy.repository annotation

--- src/main/java/org/eclipse/pass/elide/model/Policy.java
+++ src/main/java/org/eclipse/pass/elide/model/Policy.java
@@ -23,6 +23,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
 import com.yahoo.elide.annotation.Include;
@@ -59,6 +60,7 @@ public class Policy {
      * List of URIs for repositories that can satisfying this policy
      */
     @OneToMany
+    @JoinColumn(name = "Repository")
     private List<Repository> repositories = new ArrayList<>();
 
     /**
