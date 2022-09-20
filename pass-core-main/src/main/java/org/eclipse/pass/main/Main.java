7042a7286688af7eab1814c0fbef37146f6d1cca
Mark Patton <mark.patton@gmail.com>
2022-09-20 09:19:05 -0400

    Switch to using the Spring Boot integration.

--- pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
+++ pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
@@ -5,28 +5,18 @@
  */
 package org.eclipse.pass.main;
 
-import com.yahoo.elide.standalone.ElideStandalone;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
-import lombok.extern.slf4j.Slf4j;
-
-/**
- * Example app using Elide library.
- */
-@Slf4j
+@SpringBootApplication
+//@ServletComponentScan(basePackages = { "org.dataconservancy.pass.doi.service" })
+@ComponentScan(basePackages = {"org.eclipse.pass.main", "org.dataconservancy.pass.doi.service" })
+@EntityScan(basePackages = { "org.eclipse.pass.object.model" })
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        //If JDBC_DATABASE_URL is not set, we'll run with H2 in memory.
-        boolean inMemory = (System.getenv("JDBC_DATABASE_URL") == null);
-
-        Settings settings = new Settings(inMemory) {};
-
-        ElideStandalone elide = new ElideStandalone(settings);
-
-        if (inMemory) {
-            // settings.runLiquibaseMigrations();
-        }
-
-        elide.start();
+        SpringApplication.run(Main.class, args);
     }
 }
