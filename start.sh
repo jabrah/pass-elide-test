fa3df94edc000d0b80adf5c1044e46d157b354df
Mark Patton <mark.patton@gmail.com>
2022-09-26 09:30:16 -0400

    Setting properties to

--- start.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-DB_OPTS="-Djavax.persistence.schema-generation.database.action=create -Djavax.persistence.schema-generation.scripts.action=create -Djavax.persistence.schema-generation.create-database-schemas=true -Djavax.persistence.schema-generation.scripts.create-target=passdb.sql"
-
-$JAVA_HOME/bin/java $DB_OPTS -jar pass-core-main/target/pass-core-main.jar
